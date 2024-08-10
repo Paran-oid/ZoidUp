86dcf85efae0e72f4689ec3d7afb4ca42aa559c6
Aziz Hmidi <azizhmidi125@gmail.com>
2024-08-10 13:13:46 +0100

    frontend commit: -finished with the outside of the app

--- ZoidUpWEB/src/app/components/home/chat/chat.component.ts
+++ ZoidUpWEB/src/app/components/home/chat/chat.component.ts
@@ -1,15 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss',
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
+  currentUser: any = { id: 1, username: 'Aziz' };
   model: any = { username: 'Yasmin', status: 'offline since 3 days ago' };
+
+  sentMessageForm: FormGroup = new FormGroup({});
+
   messages: any[] = [
     { id: 1, userName: 'Aziz', body: 'hello how are you doing?', userId: 1 },
     {
@@ -27,9 +32,36 @@ export class ChatComponent {
     {
       id: 4,
       userName: 'Aziz',
-      body: 'We should hangout!',
+      body: 'We should watch a movie together at 9pm!',
       userId: 1,
     },
+    {
+      id: 5,
+      userName: 'Yasmine',
+      body: 'Sure thing, see you then!',
+      userId: 2,
+    },
   ];
-  currentUser: any = { id: 1, username: 'Aziz' };
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.sentMessageForm = this.fb.group({
+      send: [''],
+    });
+  }
+
+  get send() {
+    return this.sentMessageForm.get('send')?.value;
+  }
+
+  @HostListener('keydown', ['$event']) ListenForSubmit(event: KeyboardEvent) {
+    if (event.key === 'enter' && this.sentMessageForm) {
+      this.OnSubmit();
+      return;
+    }
+    return;
+  }
+
+  OnSubmit() {}
 }
--- ZoidUpWEB/src/app/components/home/panel/panel.component.ts
+++ ZoidUpWEB/src/app/components/home/panel/panel.component.ts
@@ -1,17 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { User } from '../../../models/user/user.model';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-panel',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './panel.component.html',
   styleUrl: './panel.component.scss',
 })
 export class PanelComponent implements OnInit {
   @Input() friends: User[] = [];
-  constructor() {}
 
-  ngOnInit() {}
+  searchForm: FormGroup = new FormGroup({});
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.searchForm = this.fb.group({
+      search: [''],
+    });
+  }
 }
